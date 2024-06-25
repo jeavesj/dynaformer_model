40bcf79ffaafad55c32bfa23d184b5a59cd7ee25
Yaosen Min <yaosenmin@microsoft.com>
2024-06-25 03:15:32 +0000

    update env

--- install.sh
+++ install.sh
@@ -3,27 +3,20 @@
 # Licensed under the MIT License.
 
 # install requirements
-pip install pip -U
-pip install torch==1.9.1+cu111 torchaudio -f https://download.pytorch.org/whl/cu111/torch_stable.html
+pip install 'pip<24' ninja
+conda install numpy==1.20.3 pytorch==1.10.0 torchvision==0.11.0 torchaudio==0.10.0 cudatoolkit=11.3 -c pytorch -c conda-forge -y
 # install torchaudio, thus fairseq installation will not install newest torchaudio and torch(would replace torch-1.9.1)
 pip install lmdb
-pip install torch-scatter==2.0.9 -f https://pytorch-geometric.com/whl/torch-1.9.1+cu111.html
-pip install torch-sparse==0.6.12 -f https://pytorch-geometric.com/whl/torch-1.9.1+cu111.html
-pip install torch-geometric==1.7.2
-pip install tensorboardX==2.4.1
-pip install ogb==1.3.2
-pip install rdkit-pypi==2021.9.3
+pip install torch-scatter==2.0.9 torch-sparse==0.6.12 -f https://pytorch-geometric.com/whl/torch-1.10.0+cu113.html
 pip install dgl==0.7.2 -f https://data.dgl.ai/wheels/repo.html
-pip install icecream
-pip install torchmetrics
-pip install tensorboard
-pip install setuptools==59.5.0
-pip install protobuf==3.20.1
-pip install wandb
-pip install Cython
 # fix AttributeError: module 'numpy' has no attribute 'float'
-pip install numpy==1.20.3 scipy pandas
-
+pip install torch-geometric==1.7.2 \
+            tensorboardX==2.4.1 ogb==1.3.2 \
+            rdkit-pypi==2021.9.3 icecream \
+            torchmetrics==0.9.2 tensorboard \
+            setuptools==59.5.0 protobuf==3.20.1 \
+            wandb Cython \
+            numpy==1.20.3 scipy pandas
 cd fairseq
 # if fairseq submodule has not been checkouted, run:
 # git submodule update --init --recursive
